2f8d69ad4b32535390066f8c12545b96cc4aad50
Dante Kong <jdbumgardner.ms@gmail.com>
2020-09-24 00:54:17 -0400

    len does not take generator and other bugfixes + MonstrousMccaw priority modifier + scavenging hyena priority modifier

--- hearthstone/battlebots/CardSpecificHeuristics.py
+++ hearthstone/battlebots/CardSpecificHeuristics.py
@@ -17,7 +17,7 @@ class CardHeuristic:
             self.cards_to_add = cards_to_add
 
     def get_modification(self, player: 'Player', card: 'MonsterCard'):
-        if card in self.cards_to_add:
+        if type(card) in self.cards_to_add:
             return self.modification_value(player, card)
         return 0
 
@@ -29,31 +29,45 @@ class SameTypeAdvantage(CardHeuristic):
     cards_to_add = [card_type for card_type in PrintingPress.all_types() if card_type.check_type(MONSTER_TYPES.BEAST)]
 
     def modification_value(self, player:'Player', card: 'MonsterCard'):
-        return len(player_card for player_card in itertools.chain(player.in_play, player.hand) if player_card.check_type(card.monster_type))
+        return len([player_card for player_card in itertools.chain(player.in_play, player.hand) if player_card.check_type(card.monster_type)])
 
 
 class MamasLove(CardHeuristic):
     cards_to_add = [card_type for card_type in PrintingPress.all_types() if card_type.check_type(MONSTER_TYPES.BEAST)]
 
     def modification_value(self, player:'Player', card: 'MonsterCard'):
-        return 10 * len(player_card for player_card in itertools.chain(player.in_play, player.hand) if type(player_card) == MamaBear)
+        if type(card) == MamaBear:
+            return 20
+        return 10 * len([player_card for player_card in itertools.chain(player.in_play, player.hand) if type(player_card) == MamaBear])
 
 
 class HoppingMad(CardHeuristic):
     cards_to_add = [PogoHopper]
 
     def modification_value(self, player:'Player', card: 'MonsterCard'):
-        return 5 * len(player_card for player_card in itertools.chain(player.in_play, player.hand) if type(player_card) == PogoHopper)
+        return 5 * len([player_card for player_card in itertools.chain(player.in_play, player.hand) if type(player_card) == PogoHopper])
 
 
 class DragonPayoffs(CardHeuristic):
     cards_to_add = [NadinaTheRed, RazorgoreTheUntamed]
 
     def modification_value(self, player:'Player', card: 'MonsterCard'):
-        return 50 if len(player_card for player_card in itertools.chain(player.in_play, player.hand) if player_card.check_type(MONSTER_TYPES.DRAGON)) >= 3 else 0
+        return 50 if len([player_card for player_card in itertools.chain(player.in_play, player.hand) if player_card.check_type(MONSTER_TYPES.DRAGON)]) >= 3 else 0
+
 
 class MonstrousMacawPower(CardHeuristic):
     cards_to_add = [MonstrousMacaw]
 
     def modification_value(self, player:'Player', card: 'MonsterCard'):
-        return max(player_card.tier for player_card in itertools.chain(player.in_play, player.hand) if player_card.deathrattles)
+        deathrattler_tiers = [player_card.tier for player_card in itertools.chain(player.in_play, player.hand) if player_card.deathrattles]
+        if deathrattler_tiers:
+            return max(deathrattler_tiers)
+        return 0
+
+
+class Scavengers(CardHeuristic):
+    cards_to_add = [ScavengingHyena]
+
+    def modification_value(self, player:'Player', card: 'MonsterCard'):
+        if player.tavern_tier < 3:
+            return len([player_card for player_card in itertools.chain(player.in_play, player.hand) if player_card.check_type(MONSTER_TYPES.BEAST)])
