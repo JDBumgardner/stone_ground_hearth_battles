d7509a2aa1b1f6524992e5b1e47dcc373638ab63
Jeremy Salwen <jeremysalwen@gmail.com>
2020-10-01 00:25:00 -0400

    Implement play_tcp_game

--- hearthstone/text_agent/play_tcp_game.py
+++ hearthstone/text_agent/play_tcp_game.py
@@ -7,26 +7,28 @@ from hearthstone.agent import Agent
 from hearthstone.battlebots.early_game_bot import EarlyGameBot
 from hearthstone.battlebots.priority_functions import PriorityFunctions
 from hearthstone.host import AsyncHost
+from hearthstone.text_agent.tcp import TcpTransport
 from hearthstone.text_agent.text_agent import TextAgent
 
 
 async def open_client_streams(max_players: int) -> Dict[str, Agent]:
     result = {}
 
-    loop = asyncio.get_event_loop()
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('localhost', 9998))
-    server.listen(8)
-    server.setblocking(False)
-    while len(result) < max_players:
-        conn, _ = await loop.sock_accept(server)
-        my_stream_reader = WHAT(conn)
+    server: asyncio.AbstractServer
+
+    async def serve(client_reader: asyncio.StreamReader, client_writer: asyncio.StreamWriter):
+        nonlocal server
+        transport = TcpTransport(client_reader, client_writer)
         await transport.send('Please enter your name: ')
         name = await transport.receive_line()
         name = name.strip()
         print(f"'{name}' has entered the game!")
         result[name] = TextAgent(transport)
+        if len(result) >= max_players:
+            server.close()
 
+    server = await asyncio.start_server(serve, '0.0.0.0', 9998)
+    await server.serve_forever()
     return result
 
 
