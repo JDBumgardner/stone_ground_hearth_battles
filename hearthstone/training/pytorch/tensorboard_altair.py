61f05db97685c0a584e3225d1f29ee5138d85d56
Jeremy Salwen <jeremysalwen@gmail.com>
2021-04-22 01:17:26 -0400

    Fix assertion in tensorboard altair
    Now, the rearrange logits may be larger than the number of cards, but the extra entries are ignored. This simplifies handling of batched inference, since the size of the rearrange logits will be the number of cards in the maximum of the batch.

--- hearthstone/training/pytorch/tensorboard_altair.py
+++ hearthstone/training/pytorch/tensorboard_altair.py
@@ -203,7 +203,8 @@ def plot_replay(replay: Replay, player_name: str, tensorboard: SummaryWriter, gl
             annotations.board,
             )
         rearrange_logit = step.agent_annotation.debug.permutation_logits.squeeze(0).tolist()
-        assert len(rearrange_logit) == len(annotations.board), (rearrange_logit, annotations.board)
+        assert len(rearrange_logit) >= len(annotations.board)
+        rearrange_logit = rearrange_logit[:len(annotations.board)]
         stores.append(annotations.store)
         hands.append(annotations.hand)
         boards.append(annotations.board)
